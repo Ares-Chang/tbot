1a43b1278b12b0597d6ed9734612cc06a089b7d6
Chang <areschang114@qq.com>
2024-06-03 14:24:25 +0800

    feat: 命令输入功能

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import process from 'node:process'
+import { env, stdin, stdout } from 'node:process'
+import { createInterface } from 'node:readline/promises'
 import { config } from 'dotenv'
 import OpenAI from 'openai'
 
 // 注入环境变量，引自 .env 文件
 config()
+
+const rl = createInterface({
+  input: stdin,
+  output: stdout,
+})
 
 /**
  * Open AI API Docs
@@ -11,18 +17,26 @@ config()
  */
 const openai = new OpenAI({
   baseURL: 'https://api.chatanywhere.com.cn', // 国内中转
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: env.OPENAI_API_KEY,
 })
 
-async function initChat() {
+askQuestion()
+async function askQuestion() {
+  const content = await rl.question('You: ')
+
+  rl.close()
+
   const stream = await openai.chat.completions.create({
-    messages: [{ role: 'user', content: '鲁迅和周树人有什么区别？' }],
+    messages: [
+      {
+        role: 'user',
+        content,
+      },
+    ],
     model: 'gpt-3.5-turbo',
     stream: true,
   })
 
   for await (const chunk of stream)
-    process.stdout.write(chunk.choices[0]?.delta?.content || '')
+    stdout.write(chunk.choices[0]?.delta?.content || '')
 }
-
-initChat()
