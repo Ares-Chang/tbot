61f7c65cfd9eb2931b3b6fbedddbacefa014594d
Chang <areschang114@qq.com>
2024-06-03 13:46:31 +0800

    feat: 引入 Open AI 测试通信内容

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
-export const one = 1
-export const two = 2
+import process from 'node:process'
+import { config } from 'dotenv'
+import OpenAI from 'openai'
+
+// 注入环境变量，引自 .env 文件
+config()
+
+/**
+ * Open AI API Docs
+ * @see https://platform.openai.com/docs/api-reference/streaming
+ */
+const openai = new OpenAI({
+  baseURL: 'https://api.chatanywhere.com.cn', // 国内中转
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+async function initChat() {
+  const stream = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: '鲁迅和周树人有什么区别？' }],
+    model: 'gpt-3.5-turbo',
+    stream: true,
+  })
+
+  for await (const chunk of stream)
+    process.stdout.write(chunk.choices[0]?.delta?.content || '')
+}
+
+initChat()
