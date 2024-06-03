ee01e5bee98ad8903204c22d8e051f334619cc93
Chang <areschang114@qq.com>
2024-06-03 15:44:54 +0800

    perf: 抽离可配置项为环境变量

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,13 @@ enum Role {
 // 注入环境变量，引自 .env 文件
 config()
 
+const baseURL = env.BASE_URL || 'https://api.chatanywhere.com.cn' // 国内中转
+const apiKey = env.OPENAI_API_KEY || ''
+const model = env.MODEL || 'gpt-3.5-turbo'
+const messageMax = Number(env.MESSAGE_MAX) || 10
+const userName = env.USER_NAME || 'You'
+const botName = env.BOT_NAME || 'Bot'
+
 const rl = createInterface({
   input: stdin,
   output: stdout,
@@ -26,15 +33,15 @@ const rl = createInterface({
  * @see https://platform.openai.com/docs/api-reference/streaming
  */
 const openai = new OpenAI({
-  baseURL: 'https://api.chatanywhere.com.cn', // 国内中转
-  apiKey: env.OPENAI_API_KEY,
+  baseURL,
+  apiKey,
 })
 
 const messages: Message[] = []
 
 function addMessage(role: Role, content: string) {
   // 设置历史消息上限
-  if (messages.length > 10)
+  if (messages.length > messageMax)
     messages.shift()
 
   messages.push({
@@ -48,18 +55,18 @@ while (true)
   await askQuestion()
 
 async function askQuestion() {
-  const content = await rl.question('You: ')
+  const content = await rl.question(`${userName}: `)
   addMessage(Role.user, content)
 
   checkExit(content)
 
   const stream = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+    model,
     stream: true,
     messages,
   })
 
-  stdout.write('Bot: ')
+  stdout.write(`${botName}: `)
 
   let text = ''
   for await (const chunk of stream) {
@@ -72,6 +79,7 @@ async function askQuestion() {
   addMessage(Role.bot, text)
 }
 
+// 检测退出
 function checkExit(str: string) {
   if (str !== 'exit')
     return
